ba57e17809f92ce96d24028eebd18805e12607c8
Harry <harry@HarrydeMacBook-Air.local>
2026-05-27 18:50:19 +0800

    second

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -18,22 +18,39 @@ freeStyleJob("Tools/clone-repository") {
 }
 freeStyleJob("Tools/SEED") {
     displayName("SEED")
-    parameter {
+    parameters {
         stringParam("GITHUB_NAME", "", "GitHub repository owner/repo_name '(e.g.: EpitechIT31000/chocolatine)'")
         stringParam("DISPLAY_NAME", "", "Display name for the job")
     }
     steps {
         dsl {
-            text(job ("\$DISPLAY_NAME") {
-                scm {
-                    remote {
-                        github("\$GITHUB_NAME")
+            text("""
+                def GitHub_project_URL = "git@github.com:\${GITHUB_NAME}.git"
+                job ("\${DISPLAY_NAME}") {
+                    scm {
+                        git("\${GitHub_project_URL}") {
+                            remote {
+                                github("\${GITHUB_NAME}")
+                            }
+                        }
+
+                    }
+                    properties {
+                        githubProjectUrl("\${GitHub_project_URL}")
+                    }
+                    triggers {
+                        scm("* * * * *")}
+                    wrappers {
+                        preBuildCleanup()
+                    }
+                    steps {
+                        shell("make fclean")
+                        shell("make")
+                        shell("make tests_run")
+                        shell("make clean")
                     }
                 }
-                triggers {
-                    githubpush
-                    scm("* * * * *")}
-            })
+            """)
         }
     }
 }
